264d45f15d7adee87030d94b0dcd22fdd329a8b3
MGMKLML <mgmklml@yandex.ru>
2021-05-29 19:01:24 +0300

    fix lint

--- __tests__/test.page-loader.js
+++ __tests__/test.page-loader.js
@@ -1,3 +1,14 @@
-test('test', () => {
-  expect('5').toEqual('5');
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFilePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+
+describe('test', () => {
+  test('1', () => {
+    getFilePath('test_file.txt');
+    expect(5).toBe(5);
+  });
 });
