390ccd9ffa93a4fbb2db64178cc37b8b5ca34c3c
MGMKLML <mgmklml@yandex.ru>
2021-06-03 01:45:41 +0300

    add test

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -1,17 +1,27 @@
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import fs from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+import os from 'os';
+import nock from 'nock';
 import pageLoader from '../index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+nock.disableNetConnect();
 
-const getFilePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+const url = 'https://ru.hexlet.io/courses';
+const expected = '<!DOCTYPE html><html><head></head><body></body></html>';
 
-describe('test', () => {
-  test('1', () => {
-    const expected = fs.readFileSync(getFilePath('test_file.txt'), 'utf-8');
-    const res = pageLoader();
-    expect(res.toString()).toBe(expected);
-  });
+let tempDir;
+
+beforeEach(async () => {
+  tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+test('download html', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, expected);
+
+  await pageLoader(url, tempDir);
+  const files = await fs.readdir(tempDir);
+  const result = await fs.readFile(path.join(tempDir, files[0]), 'utf-8');
+  expect(result).toBe(expected);
 });
